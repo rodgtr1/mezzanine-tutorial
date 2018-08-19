a7a983894690f0ac47ae041ae1543f174967cec8
Travis Rodgers <travisdotmedia@gmail.com>
2018-08-18 22:59:09 -0400

    added blog posts

--- myproject/urls.py
+++ myproject/urls.py
@@ -9,7 +9,7 @@ from mezzanine.core.views import direct_to_template
 from mezzanine.conf import settings
 
 # Uncomment to use blog as home page. See also urlpatterns section below.
-# from mezzanine.blog import views as blog_views
+from mezzanine.blog import views as blog_views
 
 admin.autodiscover()
 
@@ -39,7 +39,7 @@ urlpatterns += [
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+    # url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
     # ---------------------------------------------
@@ -66,7 +66,7 @@ urlpatterns += [
     # page tree in the admin if it was installed.
     # NOTE: Don't forget to import the view function too!
 
-    # url("^$", blog_views.blog_post_list, name="home"),
+    url("^$", blog_views.blog_post_list, name="home"),
 
     # MEZZANINE'S URLS
     # ----------------
